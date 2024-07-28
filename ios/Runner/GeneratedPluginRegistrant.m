e37db01d080633a5a2a1b7125726c73ad3dc0687
zackama <zacmoa2@gmail.com>
2024-07-28 16:42:28 +0300

    firstmap

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,16 +6,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<google_maps_flutter_ios/FLTGoogleMapsPlugin.h>)
-#import <google_maps_flutter_ios/FLTGoogleMapsPlugin.h>
-#else
-@import google_maps_flutter_ios;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
 }
 
 @end
