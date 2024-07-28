dbc26c2f6a37a1f4d7ffc06b7d3cd9b4eecd3003
zackama <zacmoa2@gmail.com>
2024-07-28 16:41:55 +0300

    firstmap

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 
 
 
+class mapV extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+
+}
